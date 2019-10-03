0a8f4e1c424024e26dffc75b4a0e97789943e7e7
Nils Rohe <nils.rohe@sosafe.de>
2019-10-03 15:15:45 +0200

    finished the checkpoint

--- src/db/queries.collabs.js
+++ src/db/queries.collabs.js
@@ -15,9 +15,49 @@ module.exports = {
 
     const getTheWiki = Wiki.findByPk(id);
 
-    return Promise.all([getCollabsForWiki, getTheWiki])
+    const getUserList = User.findAll();
+
+    return Promise.all([getCollabsForWiki, getTheWiki, getUserList])
       .then(result => {
         callback(null, result);
+      })
+      .catch(err => {
+        callback(err);
+      });
+  },
+
+  deleteCollab(req, callback) {
+    return Collab.findByPk(req.body.collaborator).then(collab => {
+      collab.destroy().then(deletedRecordsCount => {
+        callback(null, deletedRecordsCount);
+      });
+    });
+  },
+
+  createCollab(req, callback) {
+    let user;
+    return User.findOne({ where: { email: req.body.collaborator } })
+      .then(collabUser => {
+        user = collabUser;
+        Collab.findOne({
+          where: {
+            userId: collabUser.id,
+            wikiId: req.params.wikiId
+          }
+        })
+        .then(collab => {
+          if (collab) {
+            req.flash("notice", "You have added this user already.");
+            callback(401);
+          } else {
+            Collab.create({
+              userId: user.id,
+              wikiId: req.params.wikiId
+            }).then(collab => {
+              callback(null, collab);
+            });
+          }
+        });
       })
       .catch(err => {
         callback(err);
--- src/routes/collaborators.js
+++ src/routes/collaborators.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const collabController = require("../controllers/collabController");
 
 router.get("/wikis/:wikiId/collab", collabController.show);
+router.post("/wikis/:wikiId/collabs/delete", collabController.delete);
+router.post("/wikis/:wikiId/collabs/create", collabController.create);
 
 module.exports = router;
